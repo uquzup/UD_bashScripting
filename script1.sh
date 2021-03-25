5054a01da211ecefa395d83e3319714a215209d9
uquzup <50335456+uquzup@users.noreply.github.com>
2021-03-25 21:29:22 +0200

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+Vpython3=$(python3 --version)
+if [ "$Vpython3" > 2 ] 
+then
+echo "$Vpython3 > 2. Python 3 is better than python 2 because it supports modern technique such as AI, machine learning. Also python 3 is strongly recommended for any new development. As of January 2020, Python 2 has reached End Of Life status, meaning it will receive no further updates or bugfixes, including for security issues."
+else
+echo "$Vpython3 < 2."
+fi
